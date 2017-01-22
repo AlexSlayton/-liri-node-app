fe7bcaa3bc6ec80add432a4f81d960131edc97e3
Alex Slayton <alexslayton.home@gmail.com>
2017-01-21 20:28:33 -0600

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'T7Cl8TuwBf2dwhdaa3ncv00pu',
-  consumer_secret: 'CwnV0OTPyy2zvNULmNcQKCv8haq81zbpologyxUcyotIQsq8gl',
-  access_token_key: '384676238-LC2CBctRSsWMABy5w9TA0IyQD4ozahdf68I78eb4',
-  access_token_secret: 'AEqveEW2hCAdqCgmQviufJausixP9yguQlUEcT6VI4unc',
-}
